870d7a501a38da61c17af9dbd1f6e1b67370562f
Junior Teixeira <junior26bmx@gmail.com>
2022-10-03 12:04:48 -0300

    Atualizando a pasta

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,63 @@
-package Application;public class Program {
+package application;
+
+
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Pasta de candidatos e votos: ");
+        String path= sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            Map<String, Integer> map = new TreeMap<>();
+
+            String line = br.readLine();
+            while (line != null){
+
+                String[] fields = line.split(",");
+                String candidato = fields[0];
+                int votos = Integer.parseInt(fields[1]);
+                if (map.containsKey(candidato)){
+                    int votesSoFar = map.get(candidato);
+                    map.put(candidato, votos + votesSoFar);
+
+
+                }else {
+                    map.put(candidato, votos);
+                }
+
+
+
+                line = br.readLine();
+            }
+
+            for (String key : map.keySet()) {
+
+                System.out.println(key + ": " + map.get(key));
+            }
+
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,2 +1,50 @@
-package entities;public class Product {
+package entities;
+import application.Program;
+
+import java.util.Objects;
+
+public class Product {
+
+    private String candidato;
+    private Integer votos;
+
+    public Product(String candidato, Integer votos) {
+        this.candidato = candidato;
+        this.votos = votos;
+    }
+
+    public Product(Integer votos) {
+        this.votos = votos;
+    }
+
+    public String getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(String candidato) {
+        this.candidato = candidato;
+    }
+
+
+    public Integer getVotos() {
+        return votos;
+    }
+
+    public void setVotos(Integer votos) {
+        this.votos = votos;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return candidato.equals(product.candidato) && votos.equals(product.votos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(candidato, votos);
+    }
 }
